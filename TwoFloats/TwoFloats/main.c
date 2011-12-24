4d8b84a19e492e42dedc32bbf784637a2ff38764
Mark Holton <holtonma@gmail.com>
2011-12-23 19:03:16 -0800

    TwoFloats

---
+++ TwoFloats/TwoFloats/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by MARK HOLTON on 12/23/11.
+//  Copyright (c) 2011 RedGrind. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    float pi      = 3.14;
+    float someNum = 42;
+    
+    double sum = pi + someNum;
+    
+    printf("The sum is %f\n", sum);
+    return 0;
+}
+
