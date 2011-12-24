fa740d0d184949b968d89ed2b281c675b0b2e909
Mark Holton <holtonma@gmail.com>
2011-12-23 19:08:15 -0800

    ClassCertificates

---
+++ ClassCertificates/ClassCertificates/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  ClassCertificates
+//
+//  Created by MARK HOLTON on 12/23/11.
+//  Copyright (c) 2011 RedGrind. All rights reserved.
+//
+
+#include <stdio.h>
+
+void congratulateStudent(char *student, char *course, int numDays)
+{
+    printf("%s has done as much %s Programming as I could fit into %d days.\n", 
+           student, course, numDays);
+}
+int main (int argc, const char * argv[])
+{
+    // insert code here...
+    congratulateStudent("Mark", "Cocoa",       5);
+    congratulateStudent("Bo",   "Objective-C", 2);
+    congratulateStudent("Mike", "Python",      5);
+    congratulateStudent("Ted",  "iOS",         5);
+    
+    return 0;
+}
+
