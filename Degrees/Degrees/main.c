6efa9b05a2d95a93026d7ae8d610c7f08802854d
Mark Holton <holtonma@gmail.com>
2011-12-26 15:25:16 -0800

    add BigNerdRanch reference pdf, along with new project Degrees (another exercise)

---
+++ Degrees/Degrees/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  Degrees
+//
+//  Created by MARK HOLTON on 12/26/11.
+//  Copyright (c) 2011 RedGrind. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float convertToCelcius(int degC)
+{
+    float degF = 1.8 * degC + 32;
+    return degF;
+}
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("28 deg Celcius is: %f deg Fahrenheit \n", convertToCelcius(28));
+    float freezeInC = 0;
+    float freezeInF = convertToCelcius(freezeInC);
+    printf("Water freezes in %f deg C, which is %f deg F \n", freezeInC, freezeInF);
+    return EXIT_SUCCESS;
+}
+
