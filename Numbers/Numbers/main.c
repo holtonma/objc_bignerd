bcb97a5271de857b4cda71ac54560040b146337a
Mark Holton <holtonma@gmail.com>
2011-12-26 16:58:18 -0800

    completed chapter 6 on numbers, including challenge

--- Numbers/Numbers/main.c
+++ Numbers/Numbers/main.c
@@ -7,15 +7,32 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float testingCast(int num1, int num2){
+    float result = num1 / (float)num2;
+    return result;
+}
+
+void printFloats(){
+    double y = 12345.6789;
+    printf("y is %f\n", y);
+    printf("y is %e\n", y);
+    
+    return 0;
+}
 
 int main (int argc, const char * argv[])
 {
 
     // location 1096 of 5847 in BigNerdRanch Obj-C book
-    unsigned long x = 255;
-    printf("x is %lu.\n", x);
-    printf("In octal, x is %lo.\n", x);
-    printf("In hexadecimal, x is %lx.\n", x);
+    printf("3*3+5*2 = %d.\n", 3*3+5*2);
+    printf("11 / 3 = %f.\n", testingCast(11, 3));
+    printf("The absolute value of -5 is %d\n", abs(-5));
+    printFloats();
+    
+    printf("sine of 1 radian is %.3f \n", sin(1.0));
     return 0;
 }
 
