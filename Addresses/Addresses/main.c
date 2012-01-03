de08239f9d6393832c35be8c24b959a28cc592be
Mark Holton <holtonma@gmail.com>
2012-01-02 21:55:46 -0800

    sizeOf() int and pointer

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -17,6 +17,8 @@ int main (int argc, const char * argv[])
     printf("the int stored at addressOfI is %d\n", *addressOfI);
     *addressOfI = 89;
     printf("Now i is %d\n", i);
+    printf("An int is %zu bytes \n", sizeof(i));
+    printf("A pointer is %zu bytes \n", sizeof(addressOfI));
     printf("this function starts at %p\n", main);
     return 0;
 }
