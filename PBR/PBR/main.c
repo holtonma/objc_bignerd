79fac03ab6d6c7746c58b8d7bb2798a98d9e34fd
Mark Holton <holtonma@gmail.com>
2012-01-02 22:29:35 -0800

    more by ref... with cartesian to polar converter

--- PBR/PBR/main.c
+++ PBR/PBR/main.c
@@ -9,6 +9,28 @@
 #include <stdio.h>
 #include <math.h>
 
+void cartesianToPolar(float x, float y, double *rPtr, double *thetaPtr){
+    // Store the radius in the supplied address
+    *rPtr = sqrt(x*x + y*y);
+    
+    // calculate theta:
+    float theta;
+    if (x == 0.0){
+        if ( y == 0.0 ){
+            theta = 0.0; //theoretically undefined
+        } else if ( y > 0 ){
+          theta = M_PI_2;
+        } else {
+          theta = - M_PI_2;
+        }
+        
+    } else {
+        theta = atan(y/x);
+    }
+    // store theta in the supplied address (last param)
+    *thetaPtr = theta;
+}
+
 int main (int argc, const char * argv[])
 {
     double pi = 3.14;
@@ -20,6 +42,15 @@ int main (int argc, const char * argv[])
     
     // Find the value stored in integerPart
     printf("integerPart = %.0f, fractionPart = %.2f \n", integerPart, fractionPart);
+    
+    // onto finding theta...
+    double x = 3.0;
+    double y = 4.0;
+    double radius;
+    double angle;
+    
+    cartesianToPolar(x, y, &radius, &angle);
+    printf("(%.2f, %.2f) becomes (%.2f radians, %.2f)\n", x, y, radius, angle);
     
     return 0;
 }
