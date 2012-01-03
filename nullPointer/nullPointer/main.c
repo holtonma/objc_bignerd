1ded497b8ed99887d1a024db9a9a8824fc998440
Mark Holton <holtonma@gmail.com>
2012-01-02 22:06:41 -0800

    null example

---
+++ nullPointer/nullPointer/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  nullPointer
+//
+//  Created by MARK HOLTON on 1/2/12.
+//  Copyright (c) 2012 RedGrind. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    // insert code here...
+    float *myPointer;
+    // set myPointer to NULL for now, I'll store a pointer there
+    myPointer = NULL;
+    // Has myPointer been set?
+    if (myPointer){
+      // myPointer is not NULL
+    } else {
+      // myPointer is NULL
+    }
+    return 0;
+}
+
