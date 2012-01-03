a86ddab05ebf2957416f04b028040b042e1e6e05
Mark Holton <holtonma@gmail.com>
2012-01-02 20:33:24 -0800

    continue looked at.  location 1283

--- Coolness/Coolness/main.c
+++ Coolness/Coolness/main.c
@@ -20,10 +20,14 @@ void runWhile(){
 void runFor(){
     int i;
     for (i=0; i < 12; i++){
+        if (i % 3 == 0){
+            continue;
+        }
         printf("Checking i = %d\n", i);
         if (i + 90 == i * i){
             break;
         }
+        
     }
     printf("The answer is %d.\n", i);
     return 0;
